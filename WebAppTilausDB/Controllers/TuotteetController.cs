edc9cdff9dd8f9cf491f15c0a0f3d6bc7fbca247
Kimmen-BA <59834874+Kimmen-BA@users.noreply.github.com>
2020-01-19 14:13:07 +0200

    Add project files.

---
+++ WebAppTilausDB/Controllers/TuotteetController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebAppTilausDB.Models;
+namespace WebAppTilausDB.Controllers
+{
+    public class TuotteetController : Controller
+    {
+        // GET: Tuotteet
+        TilausDBEntities db = new TilausDBEntities();
+        public ActionResult Index()
+        {
+            if (Session["Username"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                List<Tuotteet> model = db.Tuotteet.ToList();
+                db.Dispose();
+                return View(model);
+            }
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Tuotteet tuotteet = db.Tuotteet.Find(id);
+            if (tuotteet == null) return HttpNotFound();
+            return View(tuotteet);
+            
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public ActionResult Edit([Bind(Include = "Nimi,Ahinta,Kuva")] Tuotteet tuotteet )
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tuotteet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tuotteet);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Nimi,Ahinta,Kuva")] Tuotteet tuotteet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tuotteet.Add(tuotteet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tuotteet);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Tuotteet tuotteet = db.Tuotteet.Find(id);
+            if (tuotteet == null) return HttpNotFound();
+            return View(tuotteet);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tuotteet tuotteet = db.Tuotteet.Find(id);
+            db.Tuotteet.Remove(tuotteet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
